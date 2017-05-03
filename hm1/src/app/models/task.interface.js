7953df230147e9ba93f5a0b8dcc1394ee81e6023
Flexx02 <flexx02@gmail.com>
2017-05-03 23:11:11 +0300

    Add component film

---
+++ hm1/src/app/models/task.interface.js
@@ -0,0 +1,3 @@
+"use strict";
+exports.UrgencyMap = [{ code: 'warn', name: "Срочно" }, { code: 'accent', name: "Нормально" }, { code: 'primary', name: "Не срочно" }];
+//# sourceMappingURL=task.interface.js.map
