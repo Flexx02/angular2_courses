37fec54c162a23b033d1592037dae887d95512f6
Flexx02 <flexx02@gmail.com>
2017-05-04 20:27:10 +0300

    Revert "Add component film"
    This reverts commit 7953df230147e9ba93f5a0b8dcc1394ee81e6023.

--- hm1/src/app/models/task.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.UrgencyMap = [{ code: 'warn', name: "Срочно" }, { code: 'accent', name: "Нормально" }, { code: 'primary', name: "Не срочно" }];
-//# sourceMappingURL=task.interface.js.map
